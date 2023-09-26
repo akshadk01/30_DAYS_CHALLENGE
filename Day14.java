7577a64cd57359d0c786d9ad4b67d58c85ebc015
Akshad <144759688+akshadk01@users.noreply.github.com>
2023-09-26 10:50:51 +0530

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+
+public class LongestConsecutiveSequence {
+
+    public static int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {100, 4, 200, 1, 3, 2};
+        int longestSequenceLength = longestConsecutive(array);
+        System.out.println("Length of the longest consecutive sequence: " + longestSequenceLength);
+    }
+}
