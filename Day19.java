18d1c06f7481f66fe0bedb2b71a3c24701e4c923
Akshad <144759688+akshadk01@users.noreply.github.com>
2023-10-09 21:20:49 +0530

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isArmstrong(int num) {
+        int originalNumber = num;
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, 3);
+            num /= 10;
+        }
+
+        return sum == originalNumber;
+    }
+}
